8e377782fbc4f170576792803026b178f30ee0e2
Elena Nikolaeva <elenanikolaevaya@yandex.ru>
2025-11-09 15:55:34 +0500

    Проект ShareIt. Запросы вещей и добавление вещи в ответ на запрос. Часть 3 v.2 CheckStyle (спринт 16)

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.request;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -11,7 +10,6 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import ru.practicum.shareit.user.User;
-
 import java.time.LocalDateTime;
 
 /**
